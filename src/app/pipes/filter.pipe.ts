fd63a2e0dd5baa50ed1e3a5e18d9d106ffa37f20
Eniko Forgac <eniko_forgacs@yahoo.com>
2019-09-19 17:19:43 +0200

    proba5

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -17,12 +17,22 @@ export class FirstNamePipe implements PipeTransform {
 
 @Pipe({name: 'filterLastName'})
 export class LastNamePipe implements PipeTransform{
-    transform(patients: Patient[], searchLName: string): any[] {
-    if(!patients) return [];
-    if(!searchLName) return patients;
-    searchLName = searchLName.toLowerCase();
-    return patients.filter( p => {
-        return p.lastname.includes(searchLName);
-        });
+    transform(patients: Patient[], searchLName: string, searchFName: string): any[] {
+    if(patients && patients.length){
+        return patients.filter(p => {
+            if( searchFName && p.firstname.toLowerCase().indexOf(searchFName.toLowerCase()) === -1){
+                return false;
+            }
+            if( searchLName && p.firstname.toLowerCase().indexOf(searchLName.toLowerCase()) === -1){
+                return false;
+            }
+            return true;
+        })
+    }
+    else {
+        return patients;
+    }   
     }
   }
+
+ 
