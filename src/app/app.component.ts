291560ade0bc8a8cf54229203a89cdae61627efd
Stefan <stefam1996@live.com>
2019-09-19 21:40:47 +0200

    svasta nesto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sioux-ambulanta';
+
+  token: string = localStorage.getItem("token");
+
+  constructor() { }
+
+  logout() {
+    localStorage.clear();
+    this.token = null;
+  }
 }
