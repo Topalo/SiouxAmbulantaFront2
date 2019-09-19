005022e40dc532c898085cc39c12917588ea04d0
Marko <topalvoicmarko4@gmail.com>
2019-09-19 12:42:11 +0200

    Dodali komponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { FullCalendarModule } from '@fullcalendar/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalenderComponent } from './calender/calender.component';
+import { PatientListComponent } from './patient-list/patient-list.component';
+import { PatientAllComponent } from './patient-all/patient-all.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalenderComponent
+    CalenderComponent,
+    PatientListComponent,
+    PatientAllComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
